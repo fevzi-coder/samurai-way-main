90c06fb0833181258e597d040c5b8016592472d6
Fevzi Mukhtarov <fevzi.pobeditel@gmail.com>
2023-01-24 10:40:14 +0300

    Add components Header Navbar Profile

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return(
+        <header className="header">
+            <img src="https://toppng.com/uploads/preview/design-for-logo-11549988276bxsuzsd1y1.png" alt="logo"/>
+        </header>
+    )
+}
